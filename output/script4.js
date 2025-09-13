1bcffec3a4be9434b94a6998a4a156288d7e60e5
FyslAhmd <info.faysal.32@gmail.com>
2025-09-13 23:13:02 +0600

    learn function structure

--- output/script4.js
+++ output/script4.js
@@ -2,4 +2,10 @@ const userDetails = (id, user) => {
     console.log(id, user.name, user.age);
 };
 userDetails(123, { name: "John", age: 30 });
+//function structutre
+let userDetail;
+userDetail = (id, userInfo) => {
+    console.log(`ID is ${id} and name is ${userInfo.name} and age is ${userInfo.age}`);
+};
+userDetail(456, { name: "Alice", age: 25 });
 export {};
--- src/script4.ts
+++ src/script4.ts
@@ -6,3 +6,11 @@ const userDetails = (id: myType, user: userType) => {
 };
 userDetails(123, { name: "John", age: 30 });
 
+
+//function structutre
+let userDetail : (id:myType, user: userType) => void;
+
+userDetail = (id: myType,userInfo:userType) => {
+    console.log(`ID is ${id} and name is ${userInfo.name} and age is ${userInfo.age}`);
+}
+userDetail(456, {name: "Alice", age: 25});
